b75c02943a447638c8511a72fa5bac2549c4cee3
Joel Valencia <trekkerj1@gmail.com>
2016-04-08 11:28:36 -0500

    initial import

---
+++ app/src/app/app.ts
@@ -0,0 +1,23 @@
+import {App, events, register, routing, web} from 'platypus';
+import HomeViewControl from '../viewcontrols/home/home.vc';
+
+export default class MyApp extends App {
+    constructor(router: routing.Router, config: web.IBrowserConfig) {
+        super();
+
+		config.routingType = config.STATE;
+
+        router.configure([
+            { pattern: '', view: HomeViewControl }
+        ]);
+    }
+
+    error(ev: events.ErrorEvent<Error>): void {
+        console.log(ev.error);
+    }
+}
+
+register.app('app', MyApp, [
+    routing.Router,
+    web.IBrowserConfig
+]);
