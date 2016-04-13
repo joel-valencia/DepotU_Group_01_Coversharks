02018dcddcfc9dbab8a38c5b9135c8a0bd16faf2
treeplantedbystreams <davbros88@hotmail.com>
2016-04-13 18:26:21 -0500

    nearly finished with getting started page

--- app/src/viewcontrols/howtoplay/howtoplay.vc.ts
+++ app/src/viewcontrols/howtoplay/howtoplay.vc.ts
@@ -4,7 +4,23 @@ import BaseViewControl from '../base/base.vc';
 export default class HowToPlayViewControl extends BaseViewControl {
     templateString: string = require('./howtoplay.vc.html');
 
-    context: any = {};
-}
+    context = {
+        showVideos: false,
+        showStarted: true
+    };
+    
+    startFunc(){
+           this.context.showStarted=true;
+        this.context.showVideos=false;
+        console.log("getting started");
+    }
+    
+    vidFunc(){
+              this.context.showVideos=true; 
+           this.context.showStarted=false;
+            console.log("videos");
+           }
+        // In here, manipulate the true/false values on your context that you have your plat-visibles looking to
+    }
 
 register.viewControl('howtoplay-vc', HowToPlayViewControl);
