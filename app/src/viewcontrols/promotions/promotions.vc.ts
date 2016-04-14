5c7d0d5270c8d169ca94660cbd95398246f1ca84
Jacob Gossett <jgossett121@gmail.com>
2016-04-14 13:20:55 -0500

    modal added

--- app/src/viewcontrols/promotions/promotions.vc.ts
+++ app/src/viewcontrols/promotions/promotions.vc.ts
@@ -4,7 +4,16 @@ import BaseViewControl from '../base/base.vc';
 export default class PromotionsViewControl extends BaseViewControl {
     templateString: string = require('./promotions.vc.html');
 
-    context: any = {};
+    context: any = {
+        modal: false
+    };
+    
+    toggleModal(modal: boolean) {
+        this.context.modal = true;
+    }
+    modalOff() {
+        this.context.modal = false;
+    }
 }
 
 register.viewControl('promotions-vc', PromotionsViewControl);
