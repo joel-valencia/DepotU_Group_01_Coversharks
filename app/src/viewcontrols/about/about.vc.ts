feac5305517ad4919bf79b3957d63b9f275adede
Jacob Gossett <jgossett121@gmail.com>
2016-04-15 11:41:27 -0500

    about tweaks

--- app/src/viewcontrols/about/about.vc.ts
+++ app/src/viewcontrols/about/about.vc.ts
@@ -5,6 +5,10 @@ export default class AboutViewControl extends BaseViewControl {
     templateString: string = require('./about.vc.html');
 
     context: any = {};
+    
+    subscribe() {
+        alert('Your Subscription Has Been Sent!');
+    }
 }
 
 register.viewControl('about-vc', AboutViewControl);
